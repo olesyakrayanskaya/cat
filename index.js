62bb895de8ea51c2204c5ea62aea574195eae3fe
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2022-12-20 14:00:09 +0300

    draw grass js

---
+++ index.js
@@ -0,0 +1,23 @@
+const userWidth = window.innerWidth
+const grassItemWidth = 20
+const numbGrassItems = Math.ceil(userWidth / grassItemWidth)
+const grass = document.getElementsByClassName('grass')[0]
+
+function drawGrass() {
+    for (let i = 0; i <= numbGrassItems; i++) {
+
+        let div = document.createElement('div')
+        div.className = 'grass__item'
+        grass.appendChild(div)
+
+    }
+}
+
+drawGrass()
+
+document.addEventListener('resize', drawGrass())
+
+console.log(userWidth)
+console.log(grassItemWidth)
+console.log(numbGrassItems)
+console.log(grass)
